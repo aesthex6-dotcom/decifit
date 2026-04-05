e6a7f22b713943b5b27d9901ebccece398ed072f
Himanshu Arya <aesthex6@gmail.com>
2026-04-05 11:21:04 +0530

    final

--- script.js
+++ script.js
@@ -950,3 +950,20 @@ function openPanel() {
 window.closePanel = function () {
   document.getElementById("mobileExercisePanel").classList.remove("show");
 };
+
+const searchInput = document.getElementById("exerciseSearch");
+
+searchInput.addEventListener("input", function () {
+  const value = this.value.toLowerCase();
+  const cards = document.querySelectorAll(".exercise-card");
+
+  cards.forEach((card) => {
+    const text = card.innerText.toLowerCase();
+
+    if (text.includes(value)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+});
